b85ba8a6c90dc919b23b4eb75a04d0670142c249
Peter Siao <a130142806@gmail.com>
2018-01-31 21:57:33 +0800

    hw2test2

--- homeworks/week1/hw2.js
+++ homeworks/week1/hw2.js
@@ -3,7 +3,7 @@ export const capitalize = (str) => {
   var ascii = "str".charCodeAt(0)
   if(ascii >= 97 && ascii <= 122){
   	ascii = ascii -21
-  	input[0] = string.fromCharCode(ascii)
+  	input[0] = fromCharCode(ascii)
   }
   
   str = input.join()
