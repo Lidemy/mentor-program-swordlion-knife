f21c1fc211a08d80b61653c1f1ad5d78bde72c4c
Peter Siao <a130142806@gmail.com>
2018-01-31 21:33:59 +0800

    hw1finished

--- homeworks/week1/hw1.js
+++ homeworks/week1/hw1.js
@@ -1,13 +1,8 @@
 export const stars = (n) => {
   var star = new Array(n)
   
-	for (var a = 0; a < n; a++){
-	  	if (a!=n-1 && n!=1){
-	  		star[a] = "*".repeat(a+1)+"\n"
-	  	}
-	  	else {
-	  		star[a] = "*".repeat(a+1)
-	  	}
+	for (var a = 0; a < n; a++) {
+	  	star[a] = "*".repeat(a+1)
 	  }
   return star
 }
