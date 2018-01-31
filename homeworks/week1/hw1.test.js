01d5940574b6c672dc7983d8a05a9f6e7ccfe4cd
Peter Siao <a130142806@gmail.com>
2018-01-31 17:07:59 +0800

    revisedhw1test3

--- homeworks/week1/hw1.test.js
+++ homeworks/week1/hw1.test.js
@@ -5,6 +5,6 @@ describe("hw1", () => {
     expect(stars(1)).toEqual(['*'])
   })
   it("should return correct answer when n = 3", () => {
-  	expect(stars(3)).toEqual(['*']+"\n"+['**']+"\n"+['***'])
+  	expect(stars(3)).toEqual(['*']+"\n"+['*','*']+"\n"+['*','*','*'])
   })
 })
