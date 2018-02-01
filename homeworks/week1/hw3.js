c8fab7a3e81d0ed2726762dc4e7a6bc1ef2acadd
Peter Siao <a130142806@gmail.com>
2018-02-01 14:27:52 +0800

    hw3

--- homeworks/week1/hw3.js
+++ homeworks/week1/hw3.js
@@ -1,3 +1,8 @@
 export const isPrime = (n) => {
-  return false;
+  for (var a = 2; a < n ; a++){
+     if ( n % a == 0) {
+        return false;    
+     }
+  }
+  return true;
 }
--- homeworks/week1/hw3.test.js
+++ homeworks/week1/hw3.test.js
@@ -4,4 +4,13 @@ describe("hw3", () => {
   it("should return correct answer when n = 1", () => {
     expect(isPrime(1)).toBe(false)
   })
+  it("should return correct answer when n = 1", () => {
+    expect(isPrime(17)).toBe(true)
+  })
+  it("should return correct answer when n = 1", () => {
+    expect(isPrime(13)).toBe(true)
+  })
+  it("should return correct answer when n = 1", () => {
+    expect(isPrime(2)).toBe(false)
+  })
 })
