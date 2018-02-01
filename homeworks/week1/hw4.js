de3e88c8fd5174bc97ceb901f5a1440f5712faaf
Peter Siao <a130142806@gmail.com>
2018-02-01 15:32:06 +0800

    hw4

--- homeworks/week1/hw4.js
+++ homeworks/week1/hw4.js
@@ -1,3 +1,13 @@
 export const isPalindromes = (str) => {
-  return false;
+  var input = str.split('')
+  var newinput = []
+  for (var a = str.length-1 ; a >= 0; a --){
+      newinput.push(input[a])
+  }
+  for (var b = 0 ; b < str.length ; b ++){
+  	if (input[b] != newinput[b]){
+  		return false;
+  	}
+  	return true;
+  }
 }
