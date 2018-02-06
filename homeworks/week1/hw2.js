54061e0517db8174aaf6510ce79c1c97f435a48d
Peter Siao <a130142806@gmail.com>
2018-02-06 17:12:11 +0800

    hw2

--- homeworks/week1/hw2.js
+++ homeworks/week1/hw2.js
@@ -1,4 +1,5 @@
 export const capitalize = (str) => {
 	str.split('')
-	return str.replace(str[0],str[0].toUpperCase()).join('');
+	str.replace(str[0],str[0].toUpperCase())
+	return str.join('');
  }
