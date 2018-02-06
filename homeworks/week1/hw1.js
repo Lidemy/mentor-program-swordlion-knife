b066fbb2ebbbaaa75060e2f0a30aea49736c2cd1
Peter Siao <a130142806@gmail.com>
2018-02-06 17:06:08 +0800

    hw1

--- homeworks/week1/hw1.js
+++ homeworks/week1/hw1.js
@@ -1,8 +1,8 @@
 export const stars = (n) => {
     var Array = []
     Array.fill()
-    Array.map((i , index) => '*'.repeat(index+1))
+    Array.map((n , index) => '*'.repeat(index+1))
 
-    return Array[n] 
+    return Array 
 }
 
