8ccc36aa2590459cf80c789e1340bb263dd3c0d1
Peter Siao <a130142806@gmail.com>
2018-02-06 16:44:42 +0800

    new idea

--- homeworks/week1/hw4.js
+++ homeworks/week1/hw4.js
@@ -1,11 +1,12 @@
 export const isPalindromes = (str) => {
   var input = str.split('')
   var newinput = input.reverse()
-  
+  var error = 0
+
   for (var b = 0 ; b < str.length ; b ++) {
   	if (input[b] != newinput[b]) {
-  		return false
+  		error += 1
   	} 
   }
-  return true
+  return error
 }   
