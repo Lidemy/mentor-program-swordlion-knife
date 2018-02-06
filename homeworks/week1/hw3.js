dc63bb17267d6fd0180bab3100be8d86f3d4ccde
Peter Siao <a130142806@gmail.com>
2018-02-06 17:17:12 +0800

    hw3

--- homeworks/week1/hw3.js
+++ homeworks/week1/hw3.js
@@ -1,15 +1,13 @@
 export const isPrime = (n) => {
 
-	 if (n == 1) {
-	 	return false;
-	 }
-	 else if (n == 2) {
-	 	return true;
-	 }
-  for (var a = 2; a < n ; a++) {
-     if ( n % a == 0) {
-        return false;    
-     }
-  }
-  return true;
+	if (n == 1) {
+		return false;
+	}
+	
+	for (var a = 2; a < n ; a++) {
+	    if ( n % a == 0) {
+	       return false;    
+	    }
+	}
+return true;
 }
