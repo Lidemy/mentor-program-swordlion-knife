5d0f619d97a36f94ce931126ecdcf3271d22def0
Peter Siao <a130142806@gmail.com>
2018-02-06 16:48:36 +0800

    trying

--- homeworks/week1/hw5.js
+++ homeworks/week1/hw5.js
@@ -5,7 +5,7 @@ export const add = (a, b) => {
   
   if (inputa.length > inputb.length){
     for ( var c = 0; c < inputb.length ; c++){
-     	output.push(parseInt(inputa[c])+parseInt(inputb[c]))
+     	output.push(inputa[c]+parseInt(inputb[c]))
     }
     for ( var e = inputb.length; e < inputa.length ; e++){
       output.push(parseInt(inputa[e]))
