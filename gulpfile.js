1d7d9cc13109dfd3d4805e29fcf140cc4ea4461e
Peter Siao <a130142806@gmail.com>
2018-04-24 23:07:58 +0800

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,35 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var rename = require("gulp-rename");
+var clean = require('gulp-clean');
 
 
-gulp.task('styles', function () {
+gulp.task('styles',['clean'],()=> {
     gulp.src('./gulp/*.scss')   
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('./css'))
+        .pipe(rename({
+        	suffix: '.min'
+        }))
+        .pipe(gulp.dest('./css'));
 });
 
-gulp.task('es5', function () {
+gulp.task('es5', ['clean'],()=> {
 	gulp.src('./gulp/*.js')
 		.pipe(babel({
-			presets: [['env', {outputStyle: 'compressed'}]]
+			presets: ['env']
 		}))
 		.pipe(gulp.dest('./css'))
+        .pipe(uglify())
+        .pipe(rename({
+        	suffix: '.min'
+        }))
+        .pipe(gulp.dest('./css'));
 })
+
+gulp.task('clean', ()=>{
+	return gulp.src('./css')
+		.pipe(clean());
+});
